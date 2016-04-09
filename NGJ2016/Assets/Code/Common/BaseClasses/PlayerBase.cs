37b289bc0ebaa60f5496ece4d1182f9a84cf5fb2
Dong <ddng90@gmail.com>
2016-04-09 14:54:21 +0200

    1.4

--- NGJ2016/Assets/Code/Common/BaseClasses/PlayerBase.cs
+++ NGJ2016/Assets/Code/Common/BaseClasses/PlayerBase.cs
@@ -50,6 +50,7 @@ namespace Assets.Code.Common.BaseClasses
 
             transform.right = FacingDirection;
             transform.position += MovementDirection * MovementSpeed * Time.deltaTime;
+            Camera.current.transform.position = new Vector3() { x = transform.position.x, y = transform.position.y, z = -3 };
         }
 
         public override void Init()
